4bc22a295684eb4fdb73bf2c28f83862f2c41a94
abhinavcoder <abhinav.aajkal.kumar@gmail.com>
2016-09-17 08:06:31 +0530

    PS_2 Done

--- shelf_controller.py
+++ shelf_controller.py
@@ -7,6 +7,15 @@ import numpy as np
 #additinal imported libraries
 import math
 
+def minimum(a):
+	min = 4.0
+	for i in range (70,100):
+		if a[i] < min :
+			min = a[i]
+			index = i 
+
+	return index,a[index]
+
 class StateMachineController(ReflexController):
 	"""A more sophisticated controller that uses a state machine."""
 	def __init__(self,sim,hand,dt):
@@ -22,7 +31,7 @@ class StateMachineController(ReflexController):
 		self.dy1 = -0.0125
 		self.dy2 = +0.0125
 		self.mypos = [0,0,0]
-		self.resize_data = np.zeros(360)
+		self.resize_data = np.zeros(180)
 
 	def __call__(self,controller):
 
@@ -61,44 +70,62 @@ class StateMachineController(ReflexController):
 		
 		
 		if self.state == 'idle' :
-			self.hand.setCommand([0.2, 0.2, 0.2, 0])
+			self.hand.setCommand([math.radians(25), math.radians(25), math.radians(25), 0])
 			rotate_base(controller,rotate_angle,0.1)
 			if math.ceil(sim.getTime()%self.counter) > 1 and math.ceil(sim.getTime()%self.counter) <= 2:
-				desired = se3.mul((so3.identity(),[0,0,0.05]),xform)
+				desired = se3.mul((so3.identity(),[-0.1,0.2,0.07]),xform)
 				send_moving_base_xform_linear(controller,rotate_angle,desired[1],0.5)
-				self.mypos = [0,0,0.05]
+				self.newPosX = -0.1
+				self.state = 'set_pos'
+		elif self.state == 'set_pos':
+			lidar_data = controller.sensor(33).getMeasurements()
+			self.resize_data = lidar_data[270:360] + lidar_data[0:90]
+
+			# self.resize_data = resize_data
+			if math.ceil(sim.getTime()%self.counter) > 3:
+				x_coordinate = [ i for i in range(len(self.resize_data)) ]
+				plt.plot(x_coordinate,self.resize_data,'r')
+				plt.savefig("./old.png")
+				angle , depth = minimum(self.resize_data)
+				print "Angle , Depth ",angle,depth
+				plt.show()
+				dx = depth*math.cos(math.radians(angle))
+				dy = depth*math.sin(math.radians(angle))
+				print "\n New Pos :",-0.1+dx + 0.05
+				if angle <= 70 and angle > 95 :
+					desired = se3.mul((so3.identity(),[-0.1+dx,0.2,0.07]),xform)
+				else :	
+					desired = se3.mul((so3.identity(),[-0.1+dx + 0.05,0.2,0.07]),xform)
+				send_moving_base_xform_linear(controller,rotate_angle,desired[1],0.5)
+				self.newPosX = -0.1+dx+0.05
+				#Calculate the point and go to that point ahead of obstacle
 				self.state = 'get_lidar_data'
 		elif self.state == 'get_lidar_data':
-			lidar_data = controller.sensor(33).getMeasurements()
-			self.resize_data = lidar_data
-			# self.resize_data = resize_data
-			x_coordinate = [ i for i in range(len(self.resize_data)) ]
-			if math.ceil(sim.getTime()%self.counter) > 4:
-				x_coordinate = [ i for i in range(len(self.resize_data)) ]
-				plt.plot(x_coordinate,self.resize_data)
-				# # plt.show()
-				plt.savefig("./PS_2_1.png")
+			if math.ceil(sim.getTime()%self.counter) > 4 and math.ceil(sim.getTime()%self.counter) <= 5:
+				#self.hand.setCommand([math.radians(30), math.radians(30), math.radians(30), 0])	
+				#Go Towards the object
+				desired = se3.mul((so3.identity(),[self.newPosX, 0.55,0.07]),xform)
+				send_moving_base_xform_linear(controller,rotate_angle,desired[1],0.5)
 				self.state = 'fetch'
 		elif self.state == 'fetch':
-			if math.ceil(sim.getTime()%self.counter) > 4:
-				xPos = -self.resize_data[130]*math.sin(math.radians(40))
-				yPos = self.resize_data[130]*math.cos(math.radians(40))
-				print "\n"
-				print "Destination xPos= ",self.mypos[0] - xPos," ,yPos= ",self.mypos[1] + yPos, " current= ", so3.identity()
-				#self.hand.setCommand([math.radians(30), math.radians(30), math.radians(30), 0])	
-				desired = se3.mul((so3.identity(),[self.mypos[0] - xPos , 0.3,0.05]),xform)
-				send_moving_base_xform_linear(controller,rotate_angle,desired[1],1.0)
-				# self.mypos = [self.mypos[0] + xPos,0.3,0.05]
+			if math.ceil(sim.getTime()%self.counter) > 5 and math.ceil(sim.getTime()%self.counter) <= 6:
+				controller.setPIDCommand(controller.getCommandedConfig(),[0.0]*len(controller.getCommandedConfig()))
+				self.hand.setCommand([0.2, 0.2, 0.2, 0])
+				self.state = 'grasp'
+		elif self.state == 'grasp':
+			if math.ceil(sim.getTime()%self.counter) > 6 and math.ceil(sim.getTime()%self.counter) <= 7:
+				desired = se3.mul((so3.identity(),[0, -0.15, 0.07]), xform)
+				send_moving_base_xform_linear(controller,rotate_angle,desired[1], 1.0)
+				self.state = 'backward'
+		elif self.state =='backward':
+			if math.ceil(sim.getTime()%self.counter) > 7 and math.ceil(sim.getTime()%self.counter) <= 8:
+				self.hand.setCommand([math.radians(30), math.radians(30), math.radians(30), 0])
+				self.state = 'release'
+		elif self.state == 'release':
+			#print " Time function : " + str(math.ceil(sim.getTime()%self.counter))
+			if math.ceil(sim.getTime()%self.counter) == 1.0:
+				self.state = 'idle'
 
-		# 	controller.setPIDCommand(controller.getCommandedConfig(),[0.0]*len(controller.getCommandedConfig()))
-		# 	hand.setCommand([0.2, 0.2, 0.2, 0])
-		# 	print "Closing"
-
-		# if sim.getTime() > 3 and sim.getTime() < 4:
-			
-		# 	desired = se3.mul((so3.identity(),[0,-0.3,0.1]),xform)
-		# 	send_moving_base_xform_linear(controller,rotate_angle,desired[1],0.5)
-		# 	print "Moving backward"
 
 		#need to manually call the hand emulator
 		self.hand.process({},self.dt)
